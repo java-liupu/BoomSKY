f2f0e8d1a569f5782ca41c62118399497752e743
ruochena <1131484425@qq.com>
2019-10-31 13:53:52 +0800

    lxb

--- boomsky_service/boomsky_goods_service/src/main/java/com/mr/goods/BoomskyGoodsServiceApplication.java
+++ boomsky_service/boomsky_goods_service/src/main/java/com/mr/goods/BoomskyGoodsServiceApplication.java
@@ -1,11 +1,13 @@
 package com.mr.goods;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer   // 服务端启动 端口号: 8762
+@MapperScan("com.mr.goods.mapper")
 public class BoomskyGoodsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BoomskyGoodsServiceApplication.class,args);
---
+++ boomsky_service/boomsky_goods_service/src/main/java/com/mr/goods/mapper/PmsProductMapper.java
@@ -0,0 +1,15 @@
+package com.mr.goods.mapper;
+
+import com.mr.goods.pojo.PmsProduct;
+import com.mr.goods.util.PageUtils;
+
+import java.util.List;
+
+public interface PmsProductMapper {
+
+    Integer getTotal();
+
+    List<PmsProduct> findProductList(PageUtils pa);
+
+    Integer deleteProductInfo(Integer ids);
+}
