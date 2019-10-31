d9b37143800f9632de3e71fbe39add08c2d3264d
yyl-git <2749582095@qq.com>
2019-10-31 13:46:28 +0800

    yyl

--- boomsky_service/boomsky_goods_service/src/main/java/com/mr/goods/BoomskyGoodsServiceApplication.java
+++ boomsky_service/boomsky_goods_service/src/main/java/com/mr/goods/BoomskyGoodsServiceApplication.java
@@ -1,11 +1,15 @@
 package com.mr.goods;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer   // 服务端启动 端口号: 8762
+//@ComponentScan(basePackages = {"com.mr.goods.*"})
+@MapperScan("com.mr.goods.mapper")
 public class BoomskyGoodsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BoomskyGoodsServiceApplication.class,args);
---
+++ boomsky_service/boomsky_goods_service/src/main/java/com/mr/goods/service/impl/PmsBrandServiceImpl.java
@@ -0,0 +1,69 @@
+package com.mr.goods.service.impl;
+
+import com.mr.goods.mapper.PmsBrandMapper;
+import com.mr.goods.pojo.PmsBrand;
+import com.mr.goods.service.IPmsBrandService;
+import com.mr.goods.util.CommonResult;
+import com.mr.goods.util.PageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PmsBrandServiceImpl implements IPmsBrandService {
+
+   @Autowired
+   private PmsBrandMapper brandMapper;
+
+   //获取全部品牌列表
+    @Override
+    public CommonResult<PageUtils<PmsBrand>> findBrandList(Integer pageNum, Integer pageSize) {
+        CommonResult<PageUtils<PmsBrand>> status = new CommonResult<>();
+
+        PageUtils<PmsBrand> pageBrand = new PageUtils<>();
+        pageBrand.setPageSize(pageSize);
+        pageBrand.setPageNum(pageNum);
+        //开始条数
+        pageBrand.calculate();
+        List<PmsBrand> brandList = brandMapper.findBrandList(pageBrand);
+        pageBrand.setList(brandList);
+
+        Integer total = brandMapper.findTotalNum();
+        pageBrand.setTotal(total);
+
+        pageBrand.setTotalPage(total/pageSize);
+        status.setData(pageBrand);
+
+        status.setCode(200);
+        status.setMessage("成功");
+
+        return status;
+    }
+
+    //添加品牌
+    @Override
+    public CommonResult addBrand(PmsBrand brandVO) {
+        return brandMapper.addBrand(brandVO);
+    }
+
+    //获取全部品牌列表
+
+
+//    @Override
+//    public int deleteBrand(PmsBrand brandVO) {
+//        return brandMapper.deleteBrand(brandVO);
+//    }
+//
+//    @Override
+//    public int addBrand(PmsBrand brandVO) {
+//        return brandMapper.addBrand(brandVO);
+//    }
+//
+//    @Override
+//    public PmsBrand findBrandById(PmsBrand brandVO) {
+//        return brandMapper.findBrandById(brandVO);
+//    }
+
+
+}
