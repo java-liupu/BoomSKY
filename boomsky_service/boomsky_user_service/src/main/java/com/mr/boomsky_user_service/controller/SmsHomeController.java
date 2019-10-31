e518ad34ae8a16b28d33ffda28267636fc56e72d
feng-git-8 <1260889242@qq.com>
2019-10-31 20:06:26 +0800

    lp

--- boomsky_service/boomsky_user_service/src/main/java/com/mr/boomsky_user_service/controller/SmsHomeController.java
+++ boomsky_service/boomsky_user_service/src/main/java/com/mr/boomsky_user_service/controller/SmsHomeController.java
@@ -1,12 +1,14 @@
 package com.mr.boomsky_user_service.controller;
 
 
-import com.mr.boomsky_user_service.pojo.SmsHomeRecommendSubject;
 import com.mr.boomsky_user_service.service.SmsHomeSubjectService;
+import com.mr.boomsky_user_service.util.CommonResult;
 import com.mr.boomsky_user_service.util.JyyData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  *  调用  专题推荐 接口
@@ -18,12 +20,20 @@ public class SmsHomeController {
     @Autowired
     private SmsHomeSubjectService smsHomeSubjectSer;
 
+    // 调用 查询专题列表 接口
     @ResponseBody
     @RequestMapping(value = "/list",method = RequestMethod.GET)
     public JyyData list(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
                         @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize){
 
         return smsHomeSubjectSer.findHomeSubjectList(pageNum,pageSize);
+    }
+
+    // 删除(物理删除)
+    @ResponseBody
+    @RequestMapping(value = "/delete",method = RequestMethod.POST)
+    public CommonResult delete(@RequestParam List<Long> ids){
+        return smsHomeSubjectSer.delete(ids);
     }
 
 }
--- boomsky_service/boomsky_user_service/src/main/java/com/mr/boomsky_user_service/service/SmsHomeSubjectService.java
+++ boomsky_service/boomsky_user_service/src/main/java/com/mr/boomsky_user_service/service/SmsHomeSubjectService.java
@@ -1,10 +1,9 @@
 package com.mr.boomsky_user_service.service;
 
 
-import com.mr.boomsky_user_service.pojo.SmsHomeRecommendSubject;
+import com.mr.boomsky_user_service.util.CommonResult;
 import com.mr.boomsky_user_service.util.JyyData;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,4 +16,6 @@ public interface SmsHomeSubjectService {
     @RequestMapping(value = "/home/recommendSubject/list",method = RequestMethod.GET)
     JyyData findHomeSubjectList(@RequestParam Integer pageNum, @RequestParam Integer pageSize);
 
+    @RequestMapping(value = "/home/recommendSubject/delete",method = RequestMethod.POST)
+    CommonResult delete(@RequestParam List<Long> ids);
 }
